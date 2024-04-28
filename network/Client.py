2989b735006b6103aec01d3d04a21d1769a2f3ef
paul0817 <100745610+paul0817@users.noreply.github.com>
2024-04-28 23:17:11 +0900

    feat: 소켓 송수신 코드

---
+++ network/Client.py
@@ -0,0 +1,19 @@
+import socket
+ 
+s = socket.socket()
+host = socket.gethostname() 
+port = 12345               
+ 
+s.connect((host, port))
+f = open('1.png','rb')
+print ('Sending...')
+l = f.read(8096)
+while (l):
+    print ('Sending...')
+    s.send(l)
+    l = f.read(8096)
+f.close()
+print ('Done Sending')
+s.shutdown(socket.SHUT_WR)
+print (s.recv(8096))
+s.close                    
---
+++ network/Server.py
@@ -0,0 +1,21 @@
+import socket              
+ 
+s = socket.socket()        
+host = socket.gethostname() 
+port = 12345               
+s.bind((host, port))       
+f = open('2.png','wb')
+s.listen(5)                
+while True:
+    c, addr = s.accept()    
+    print ('Got connection from', addr)
+    print ('Receiving...')
+    l = c.recv(8096)
+    while (l):
+        print ('Receiving...')
+        f.write(l)
+        l = c.recv(8096)
+    f.close()
+    print ('Done Receiving')
+    c.send('Thank you for connecting')
+    c.close()               
