f0f6254e5d4d60c48afd70c0dfccd4414edcd397
yujin <qrt365@naver.com>
2024-05-26 19:29:32 +0900

    comment: 코드 주석 추가

--- main/voip/receiver.py
+++ main/voip/receiver.py
@@ -6,20 +6,22 @@ import wave
 import random
 import time
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 8000
+FORMAT = pyaudio.paInt16 # 오디오 포맷
+CHANNELS = 1 # 오디오 채널 수
+RATE = 8000 # 샘플링 레이트
+CHUNK = 1024 # 처리할 프레임 수
 
-PORT = 9001
-SENDER_CONTROL_PORT = 9002  # 송신자 제어 메시지 포트
-RECEIVER_CONTROL_PORT = 9003  # 수신자 제어 메시지 포트
+PORT = 9001 # 포트 번호
+SENDER_CONTROL_PORT = 9002 # 송신자 제어 메시지 포트
+RECEIVER_CONTROL_PORT = 9003 # 수신자 제어 메시지 포트
 SENDER_IP = '10.10.1.90' # 송신자 IP 주소
 
 def receive_audio():
+    # PyAudio 객체 생성 및 스트림 초기화
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
     
+    # UDP 소켓 생성
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     control_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('0.0.0.0', PORT))
@@ -27,6 +29,7 @@ def receive_audio():
     
     print("수신 대기")
     
+    # 출력 오디오 파일을 작성하기 위한 wave 객체 생성
     wf = wave.open("output.wav", 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -57,8 +60,10 @@ def receive_audio():
             # 패킷 지연 시뮬레이션 (0~100ms 랜덤 지연)
             time.sleep(random.uniform(0, 0.1))
             data = zlib.decompress(compressed_data)
-            wf.writeframes(data)
-            stream.write(data)
+            wf.writeframes(data) # 출력 오디오 데이터를 파일에 기록
+            stream.write(data) # 오디오 데이터를 출력 장치로 전송
+
+            # 다음 패킷 수신
             compressed_data, _ = sock.recvfrom(2048)
     except KeyboardInterrupt:
         control_sock.sendto(b"END", (SENDER_IP, SENDER_CONTROL_PORT))
--- main/voip/sender.py
+++ main/voip/sender.py
@@ -6,27 +6,30 @@ import wave
 import random
 import time
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 8000
+FORMAT = pyaudio.paInt16 # 오디오 포맷
+CHANNELS = 1 # 오디오 채널 수
+RATE = 8000 # 샘플링 레이트
+CHUNK = 1024 # 처리할 프레임 수
 
-PORT = 9001
-SENDER_CONTROL_PORT = 9002  # 송신자 제어 메시지 포트
-RECEIVER_CONTROL_PORT = 9003  # 수신자 제어 메시지 포트
-RECEIVER_IP = '10.10.1.90'  # 수신자 IP 주소
+PORT = 9001 # 포트 번호
+SENDER_CONTROL_PORT = 9002 # 송신자 제어 메시지 포트 번호
+RECEIVER_CONTROL_PORT = 9003 # 수신자 제어 메시지 포트 번호
+RECEIVER_IP = '10.10.1.90' # 수신자 IP 주소
 
 def send_audio():
+    # PyAudio 객체 생성 및 스트림 초기화
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
     
+    # UDP 소켓 생성
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     control_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     control_sock.bind(('0.0.0.0', SENDER_CONTROL_PORT))
     server_address = (RECEIVER_IP, PORT)
     
-    print("송신 대기")
+    print("송신 시작")
     
+    # 입력 오디오 파일을 작성하기 위한 wave 객체 생성
     wf = wave.open("input.wav", 'wb')
     wf.setnchannels(CHANNELS)
     wf.setsampwidth(p.get_sample_size(FORMAT))
@@ -54,7 +57,7 @@ def send_audio():
     try:
         while True:
             data = stream.read(CHUNK)
-            wf.writeframes(data)
+            wf.writeframes(data) # 입력 오디오 데이터를 파일에 기록
             compressed_data = zlib.compress(data)
             # 패킷 손실 시뮬레이션 (10% 확률로 패킷 손실)
             if random.random() > 0.1:
@@ -64,6 +67,7 @@ def send_audio():
             else:
                 # 송신된 패킷 손실 시 빈 패킷을 보냄
                 sock.sendto(zlib.compress(bytes([0]*CHUNK)), server_address)
+            # 첫 번째 패킷 전송 시 연결 완료 메시지 전송
             if first_packet:
                 control_sock.sendto(b"START", (RECEIVER_IP, RECEIVER_CONTROL_PORT))
                 first_packet = False
